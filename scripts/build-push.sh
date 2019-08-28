a94b189a78895b12cc5ee346c6fa6060df3a8a46
sankar <>
2019-08-28 15:57:05 -0400

    Removed docker compose yml file and created dockerfile.build.sh instead

--- scripts/build-push.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-
-docker-compose -f docker-compose.build.yml build
-
-docker-compose -f docker-compose.build.yml push
